e796621ea1da6f00a2705c6547e97dcffd85154c
Alperen Yıldız <alperenyildiz@Alperen-MacBook-Pro.local>
2021-06-29 17:21:49 +0300

    started working on api itself

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import json
 from datetime import datetime
@@ -8,23 +8,27 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/courses.db')
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
 
-Base = declarative_base()
-Base.query = db_session.query_property()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///courses.db'
 api = Api(app)
 
 db = SQLAlchemy(app)
 
-class course(db.Model):
-    name = db.Column(db.String(120), unique=False, nullable=False)
-    crn = db.Column(db.String(120), unique=False, nullable=False, primary_key=True)
-    code = db.Column(db.String(120), unique=False, nullable=False)
+
+class Course(db.Model):
+    name = db.Column(db.String)
+    crn = db.Column(db.String, primary_key=True)
+    code = db.Column(db.String)
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    crn = db.Column(db.String, db.ForeignKey('Course.crn'))
+    day = db.Column(db.String)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
 
 class Hello(Resource):
     def get(self):
@@ -34,23 +38,39 @@ class Hello(Resource):
         }
         return response, 200
 
-class Course(Resource):
-    def get(self,course):
-        try:
-            mylist = course.split("_")
-            mylist[0] = mylist[0].upper()
+class course(Resource):
+    def post(self): #course code to all sections
+        posted_data = request.get_json()
+        code = posted_data["code"]
 
-            parsed_course = mylist[0] + " " + mylist[1]
+        sections = Course.query.filter_by(code=code).all()
 
-            return data[parsed_course], 200
+        course_info = list()
+        for section in sections:
+            crn = section.crn
+            schedule = Schedule.query.filter_by(crn = crn).all()
+            course_info.append({
+                "crn":crn,
+                "schedule": [
+                    {
+                        "day":time.day,
+                        "start_time":str(time.start_time),
+                        "end_time":str(time.end_time)
+                    } for time in schedule
+                ]
+            })
+        
+        return jsonify ({
+            "course_name": sections[0].name,
+            "code":code,
+            "sections":course_info
+        })
             
-        except:
-            return "error", 201
 
 
 
 api.add_resource(Hello,"/")
-api.add_resource(Course,"/course/<string:course>")
+api.add_resource(course,"/course")
 
 
 if __name__ == "__main__":
